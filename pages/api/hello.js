ff24edf2bb4377d34d5ba10ab9a93f92f5711ef9
Yen Lin <e19970722@gmail.com>
2024-11-10 15:36:00 +0800

    connect to database

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,13 @@
 import { db } from '@vercel/postgres';
  
 export default async function handler(request, response) {
-  const client = await db.connect();
+  const client = await db.connect({
+    url: process.env.POSTGRES_URL,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+  });
  
   try {
     await client.sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) );`;
